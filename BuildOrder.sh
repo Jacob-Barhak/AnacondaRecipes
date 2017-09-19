f43813884ce337d8e24ff8f04fa115d38c02662e
Jacob Barhak <jacob.barhak@gmail.com>
2017-09-19 03:46:15 -0500

    Build cookiecutter-django and channels
    Recipes with dependencies to build cookiecutter-django and channels

---
+++ BuildOrder.sh
@@ -0,0 +1,17 @@
+conda build hypothesis --user jacob-barhak -c jacob-barhak -c anaconda
+conda build binaryornot --user jacob-barhak -c jacob-barhak -c anaconda
+conda build chai --user jacob-barhak -c jacob-barhak -c anaconda
+conda build arrow --user jacob-barhak -c jacob-barhak -c anaconda
+conda build freezegun --user jacob-barhak -c jacob-barhak -c anaconda
+conda build jinja2 --user jacob-barhak -c jacob-barhak -c anaconda
+conda build jinja2-time --user jacob-barhak -c jacob-barhak -c anaconda
+conda build poyo --user jacob-barhak -c jacob-barhak -c anaconda
+conda build flake8 --user jacob-barhak -c jacob-barhak -c anaconda
+conda build whichcraft --user jacob-barhak -c jacob-barhak -c anaconda
+conda build cookiecutter --user jacob-barhak -c jacob-barhak -c anaconda
+conda build cookiecutter-django  --user jacob-barhak -c jacob-barhak -c anaconda
+conda build asgiref  --user jacob-barhak -c jacob-barhak -c anaconda
+conda build txaio --user jacob-barhak -c jacob-barhak -c anaconda
+conda build autobahn --user jacob-barhak -c jacob-barhak -c anaconda
+conda build daphne --user jacob-barhak -c jacob-barhak -c anaconda
+conda build channels --user jacob-barhak -c jacob-barhak -c anaconda
---
+++ README.md
@@ -0,0 +1,25 @@
+Anaconda Build Recipes
+======================
+
+This repository contains conda build recipes made in part for Anaconda Inc.
+
+End packages were:
+1. cookiecutter-django
+2. channels
+
+The packages can be build on windows 10x64 and Centos 6.9x64 using the 
+BuildOrder.bat and BuildOrder.sh. Please replace the channel for build.
+
+Conda-build 3 was used with Anaconda 4. 
+
+Some of recipes were copied from conda-forge to satisfy dependencies.
+
+
+License
+-------
+BSD 3-clause
+see License.txt
+
+
+
+
